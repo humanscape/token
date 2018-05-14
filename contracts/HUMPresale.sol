59f01bc3df672dff9ef32fe2d46ca27dfdf3b999
byungha kim <>
2018-05-14 21:52:27 +0900

    Add bonus token in presale
    remove start number / end number
    add uint256 _individualCapEther parameter for presale constructor. It get the individual cap with ether amount and convert it to wei and pass it to IndividuallyCappedCrowdsale constructor. It will be more easier than pass it by wei amount.
    add mapping(address => uint256) public bonusTokens. It contains bonus token of each contributors. This contract will archive those contributors list in contributors array, and after persale is closed, it will be distributed.

--- contracts/HUMPresale.sol
+++ contracts/HUMPresale.sol
@@ -2,40 +2,64 @@ pragma solidity ^0.4.23;
 
 import "./crowdsale/WhitelistedCrowdsale.sol";
 import "./crowdsale/CappedCrowdsale.sol";
-import "./crowdsale/MintedCrowdsale.sol";
 import "./crowdsale/IndividuallyCappedCrowdsale.sol";
 import "./HUMToken.sol";
 
 
 contract HUMPresale is WhitelistedCrowdsale, CappedCrowdsale, IndividuallyCappedCrowdsale {
-  uint256 public startNumber;
-  uint256 public endNumber;
   uint256 public constant minimum = 100000000000000000; // 0.1 ether
 
+  mapping(address => uint256) public bonusTokens;
+  uint256 public bonusPercent;
+  address[] public contributors;
+
+  event DistrubuteBonusTokens(address sender);
+
+  bool isOnSale = false;
+
   constructor (
-    uint256 _startNumber,
-    uint256 _endNumber,
     uint256 _rate,
+    uint256 _bonusPercent,
     address _wallet,
     HUMToken _token,
-    uint256 _cap
+    uint256 _cap,
+    uint256 _individualCapEther
   ) 
     public
     Crowdsale(_rate, _wallet, _token)
     CappedCrowdsale(_cap)
-    IndividuallyCappedCrowdsale(100 * (10 ** 18))  // 100 ether
+    IndividuallyCappedCrowdsale(_individualCapEther.mul(10 ** 18))
   { 
-    startNumber = _startNumber;
-    endNumber = _endNumber;
+    bonusPercent = _bonusPercent;
+  }
+
+  function _processPurchase(address _beneficiary, uint256 _tokenAmount) internal {
+    super._processPurchase(_beneficiary, _tokenAmount);
+    if (bonusTokens[_beneficiary] == 0) {
+      contributors.push(_beneficiary);
+    }
+    bonusTokens[_beneficiary] = bonusTokens[_beneficiary].add(_tokenAmount.mul(bonusPercent).div(100));
   }
 
   function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal isWhitelisted(_beneficiary) {
     super._preValidatePurchase(_beneficiary, _weiAmount);
 
     bool isOverMinimum = _weiAmount >= minimum;
-    bool isOnSale = block.number >= startNumber && block.number <= endNumber;
 
     require(isOverMinimum && isOnSale);
+  }
+
+  function distributeBonusTokens() public onlyAdminOrAdvisor {
+    require(!isOnSale);
+
+    for (uint i = 0; i < contributors.length; i++) {
+      if (bonusTokens[contributors[i]] > 0) {
+        token.transfer(contributors[i], bonusTokens[contributors[i]]);
+        bonusTokens[contributors[i]] = 0;
+      }
+    }
+
+    emit DistrubuteBonusTokens(msg.sender);
   }
 
 }
