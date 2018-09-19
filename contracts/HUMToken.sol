9159b76f731aa8c80d43def2e399c4f17a5e9c33
David Choi <david@humanscape.io>
2018-09-19 18:35:03 +0900

    Change initial supply(2,500,000,000 HUM -> 125,000,000,000 HUM)

--- contracts/HUMToken.sol
+++ contracts/HUMToken.sol
@@ -17,7 +17,7 @@ contract HUMToken is MintableToken, BurnableToken, Blacklisted {
   string public constant symbol = "HUM"; // solium-disable-line uppercase
   uint8 public constant decimals = 18; // solium-disable-line uppercase, // 18 decimals is the strongly suggested default, avoid changing it
 
-  uint256 public constant INITIAL_SUPPLY = 2500 * 1000 * 1000 * (10 ** uint256(decimals)); // 2,500,000,000 HUM
+  uint256 public constant INITIAL_SUPPLY = 125000 * 1000 * 1000 * (10 ** uint256(decimals)); // 125,000,000,000 HUM
 
   bool public isUnlocked = false;
   
